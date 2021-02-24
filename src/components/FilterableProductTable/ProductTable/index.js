cb8d02d31e66a7f7ec7cef4aba0707fa9f712522
Daniel <danielkarpen97@gmail.com>
2021-02-23 18:28:28 -0600

    update products from api

--- src/components/FilterableProductTable/ProductTable/index.js
+++ src/components/FilterableProductTable/ProductTable/index.js
@@ -1,3 +1,17 @@
-const ProductTable = () => <table></table>;
+import api from "api";
+import { useEffect, useState } from "react";
+const ProductTable = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      // Await results of reading streams as JSON
+      const productData = await api.index();
+      setProducts(() => productData);
+    })();
+  });
+
+  return <table></table>;
+};
 
 export default ProductTable;
